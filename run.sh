5235bb3654be7c6d124e7f64c2eebacceffc3a49
D. Lisboa Giglioli <d.lisboa.giglioli@avanade.com>
2020-04-04 03:04:52 -0300

    Correção run

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-echo adonis.js >> results.txt ;
+echo ------------------------------------ >> results.txt;
+
+echo adonis.js test: >> results.txt ;
 cd yardstick;
 npm install;
 npm run serve:dev & sleep 10;
@@ -8,37 +10,37 @@ pkill -f server.js ;
 sleep 5;
 
 
-echo fastify  >> results.txt ;
+echo fastify test: >> results.txt ;
 node fastify.js & sleep 5 ;
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f server.js ;
 sleep 5;
 
-echo express >> results.txt ;
+echo express test:>> results.txt ;
 node expressserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f expressserver ;
 sleep 5 
 
-echo hapi >> results.txt ;
+echo hapi test: >> results.txt ;
 node hapiserver.js & sleep 5 ;
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f hapiserver ;
 sleep 5 
 
-echo node_rawhttp >> results.txt ;
+echo node_rawhttp test: >> results.txt ;
 node rawnodeserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f rawnodeserver ;
 sleep 5 
 
-echo restify >> results.txt ;
+echo restify test:>> results.txt ;
 node restifyserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f restifyserver ;
 sleep 5 
 
-echo koa2 >> results.txt ;
+echo koa2 test: >> results.txt ;
 node koa2server.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f koa2server ;
